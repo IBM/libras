a4cb3c45f5dcb2b0a2e727af4717b0f6e41cf4b3
Filipe Correa <filipecorrea@br.ibm.com>
2020-08-11 10:15:19 -0300

    Remove unused routes

--- src/routes/data/index.js
+++ src/routes/data/index.js
@@ -10,12 +10,6 @@ const router = express.Router()
 const methods = require('requireindex')(__dirname)
 
 router.route('/data')
-  .get(methods.list)
   .post(methods.post)
-
-router.route('/data/:id')
-  .get(methods.get)
-  .put(methods.put)
-  .delete(methods.delete)
 
 module.exports = router
