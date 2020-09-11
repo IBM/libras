9dea9b1836d4dab57bc705aa489799b7cedea27d
Filipe Correa <filipecorrea@br.ibm.com>
2020-09-11 15:50:11 -0300

    Remove LIBRAS data

--- data/import.sh
+++ data/import.sh
@@ -4,8 +4,3 @@
 mongoimport --host mongo --db asl --collection signed --drop --type json --file signed.json --jsonArray
 mongoimport --host mongo --db asl --collection spoken --drop --type json --file spoken.json --jsonArray
 mongoimport --host mongo --db asl --collection translations --drop --type json --file translations.json --jsonArray
-
-# LIBRAS
-mongoimport --host mongo --db libras --collection signed --drop --type json --file signed.json --jsonArray
-mongoimport --host mongo --db libras --collection spoken --drop --type json --file spoken.json --jsonArray
-mongoimport --host mongo --db libras --collection translations --drop --type json --file translations.json --jsonArray
